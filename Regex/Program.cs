5d3932d0f5b2609236221d9543a126e0abbd487c
threepolesoft <threepolesoft@gmail.com>
2023-06-17 21:04:41 +0600

    Add project files.

---
+++ Regex/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string input = "Course fee (400)";
+
+            string output= "";
+            //output= Regex.Replace(input, @"[\d-]", string.Empty);
+
+            output = Regex.Replace(input, "[A-Za-z ()]", string.Empty);
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+
+        }
+    }
+}
